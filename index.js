666857259415a08ac1334cfe6bc4b021f1e67b50
alamin-merndev <alaminnishat@gmail.com>
2022-11-18 11:57:24 +0600

    Service Added by post method & Send it mongo database

--- index.js
+++ index.js
@@ -11,18 +11,44 @@ app.use(cors());
 app.use(express.json());
 
 //mongodbapi
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.flmxcne.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.rfyyfuu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-console.log(uri);
 
+//mongodb try function start---
+async function run() {
+  try {
+    const serviceCollection = client.db("my-life-guide").collection("services");
+    const reviewsCollection = client.db("my-life-guide").collection("reviews");
+
+    app.post("/add", async (req, res) => {
+      const service = req.body;
+      const result = await serviceCollection.insertOne(service);
+      res.send(result);
+      console.log(result);
+    });
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.log(error));
+//mongodb try function end---
+
+// Express
 app.listen(port, () => {
   console.log("Server running on port", port);
 });
 
 app.get("/", (req, res) => {
-  res.send("Server Responded");
+  res.send("Server Running");
 });
